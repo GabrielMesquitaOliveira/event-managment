5feda30f183c8f9beef401ec534aca6a8ba3c358
gabriel <gabrielmesquita0503@gmail.com>
2024-11-25 11:52:07 -0300

    Custom Artisan Command

---
+++ app/Console/Commands/SendEventReminders.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\Event;
+use Illuminate\Console\Command;
+use Illuminate\Support\Str;
+
+
+class SendEventReminders extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:send-event-reminders';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Send notificatios to all event attendes that event starts soon';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        $events = Event::with('attendees.user')
+            ->whereBetween('start_time', [now(), now()->addDay()])
+            ->get();
+
+        $eventCount = $events->count();
+        $eventsLabel = Str::plural('event', $eventCount);
+
+        $this->info('Found ' . $eventCount . ' ' . $eventsLabel . ' that start soon');
+
+        $events->each(
+            fn($event) => $event->attendees->each(
+                fn($attendee) => $this->info('Notifying ' . $attendee->user_id . ' about ' . $event->name)
+            )
+        );
+    }
+}
